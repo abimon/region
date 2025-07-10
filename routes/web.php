c0863d22da9a7ed775f8611ad60cb33ac9a58b38
abimon <eddyabby59@gmail.com>
2025-07-10 12:55:37 +0300

    instructor's name

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
     return redirect('/dashboard');
 });
 
