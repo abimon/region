3a221ba71fb18c837b3c8e821b4ac019b2550754
abimon <eddyabby59@gmail.com>
2025-07-13 18:41:04 +0300

    enrollments management

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LessonController;
 use Illuminate\Support\Facades\Auth;
@@ -20,5 +21,6 @@ Route::middleware('auth')->group(function () {
     Route::resources([
         'articles' => ArticleController::class,
         'lessons' => LessonController::class,
+        'enrollmets'=>EnrollmentController::class
     ]);
 });
