c79301376f0be74ad58defa3cffc111b60fc81eb
abimon <eddyabby59@gmail.com>
2025-09-28 14:07:29 +0300

    user profile update

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -128,7 +128,7 @@
         <div class="col-md-6 mb-2">
             <div class="card p-3">
                 <h2 class="text-center">Change Password</h2>
-                <form method="POST" action="{{ route('clients.update', $user->id) }}" enctype="multipart/form-data">
+                <form method="POST" action="{{ route('users.update', $user->id) }}" enctype="multipart/form-data">
                     @csrf
                     @method('PUT')
                     <div class="mb-3">
