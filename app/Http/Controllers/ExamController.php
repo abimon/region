08bcb57e5e3ff67e59cdfe1ae3f09cecc62eae20
abimon <eddyabby59@gmail.com>
2025-09-28 13:49:31 +0300

    enter marks only where user has any marks

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -31,9 +31,9 @@ class ExamController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store()
     {
-        dd($request->all());
+        dd(request());
         try {
             $users = User::all();
 
