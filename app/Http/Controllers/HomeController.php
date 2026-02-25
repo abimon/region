637bf830530891537f53a0c6741ce1a599c99032
abimon <eddyabby59@gmail.com>
2026-02-25 23:58:57 +0300

    redo of controlleres

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Announcement;
 use App\Models\Assigment;
+use App\Models\ChurchClass;
 use App\Models\ClassMember;
 use App\Models\Lesson;
 use Illuminate\Support\Facades\Auth;
@@ -14,7 +15,14 @@ class HomeController extends Controller
         return view("home");
     }
     public function dashboard($role){
-        $classes = ClassMember::where([['user_id', Auth::id()],['role',$role]])->pluck('id');
+        if($role=='student'){
+            $role = ['Member'];
+        }elseif($role=='teacher'){
+            $role = ['Instructor','Assessor','Director','Ass. Director','Elder'];
+        }else{
+            $role = ['Parent'];
+        }
+        $classes = ClassMember::where('user_id', Auth::id())->whereIn('role', $role)->pluck('id');
         if($classes->isEmpty()){
             if(request()->is('api/*')){
                 return response()->json(['status'=>false,'message' => 'You are not enrolled in any class', 'lessons' => [], 'assignments' => [], 'announcements' => []], 200);
@@ -27,7 +35,7 @@ class HomeController extends Controller
         $assignments = Assigment::whereIn('class_id', $classes)->where('due_by', '>=', date('Y-m-d'))->get();
         $announcements = Announcement::whereIn('class_id', $classes)->where('due_by', '>=', date('Y-m-d'))->get();
         if(request()->is('api/*')){
-            return response()->json(['status'=>true,'message' => 'Welcome to the dashboard', 'lessons' => $lessons, 'assignments' => $assignments, 'announcements' => $announcements], 200);
+            return response()->json(['status'=>true,'message' => 'Welcome to the dashboard', 'lessons' => $lessons, 'assignments' => $assignments, 'announcements' => $announcements,'classes'=>ChurchClass::whereIn('id', $classes)->get()], 200);
         }
         return view("dashboard", compact('lessons', 'assignments', 'announcements'));
     }
