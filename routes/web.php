56e161357b1ecc1a3b8de8f10f32d02bd9eff81d
abimon <eddyabby59@gmail.com>
2026-03-08 20:17:15 +0300

    get class members

--- routes/web.php
+++ routes/web.php
@@ -32,12 +32,18 @@ Route::middleware('auth')->group(function () {
         'lessons' => LessonController::class,
         'users'=>UserController::class,
         'churches'=>ChurchController::class,
-        'members'=> ClassMemberController::class
     ]);
     Route::middleware(Assessor::class)->group(function(){
         Route::resources([
             'exams'=>ExamController::class,
         ]);
+    });
+    Route::controller(ClassMemberController::class)->prefix('/members')->group(function () {
+        Route::get('/{id}', 'index');
+        Route::get('/{id}/show', 'show');
+        Route::get('/{id}/create', 'create');
+        Route::post('/{id}/store', 'store');
+        Route::get('/{id}/edit/{classMember}', 'edit');
     });
     Route::get('/sendUserEmail/{id}',[ExamController::class,'sendUserEmail']);
 });
