60a26793b5f4e3de2ea93820f5654dbf4cbdbf9a
abimon <eddyabby59@gmail.com>
2026-03-22 12:28:00 +0300

    send bulk emails

--- app/Http/Controllers/LessonController.php
+++ app/Http/Controllers/LessonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\RegionEmail;
 use App\Models\ClassMember;
 use App\Models\Lesson;
 use App\Models\LessonResource;
@@ -15,10 +16,7 @@ class LessonController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        
-    }
+    public function index() {}
 
     /**
      * Show the form for creating a new resource.
@@ -35,27 +33,27 @@ class LessonController extends Controller
     {
         try {
             Lesson::create([
-                'class_id'=>request('class_id'),
-                'title'=>request('title'),
-                'description'=>request('description'),
-                'instructor'=>request('instructor'),
-                'date'=>request('date'),
-                'venue'=>request('venue'),
-                'venue_type'=>request('venue_type'),
-                'comments'=>request('comments')??null,
-                'created_by'=>Auth::id()
+                'class_id' => request('class_id'),
+                'title' => request('title'),
+                'description' => request('description'),
+                'instructor' => request('instructor'),
+                'date' => request('date'),
+                'venue' => request('venue'),
+                'venue_type' => request('venue_type'),
+                'comments' => request('comments') ?? null,
+                'created_by' => Auth::id()
             ]);
-            $content = request('title').' class has been scheduled for '.Carbon::parse(request('date'))->toFormattedDateString().'. The class '. request('venue_type')=='online'?'link':'venue'. ' will be '.request('venue');
-            $this->sendEmail($content,'New Class', request('class_id'));
-            if(request()->is('api/*')){
-                return response()->json(['message'=>'Lesson created successfully'],201);
+            $content = request('title') . ' class has been scheduled for ' . Carbon::parse(request('date'))->toFormattedDateString() . '. The class ' . request('venue_type') == 'online' ? 'link' : 'venue' . ' will be ' . request('venue');
+            $this->sendEmail($content, 'New Class', request('class_id'));
+            if (request()->is('api/*')) {
+                return response()->json(['message' => 'Lesson created successfully'], 201);
             }
-            return back()->with('success','Lesson created successfully');
+            return back()->with('success', 'Lesson created successfully');
         } catch (\Throwable $th) {
-           if(request()->is('api/*')){
-                return response()->json(['message'=>'Something went wrong'],400);
+            if (request()->is('api/*')) {
+                return response()->json(['message' => 'Something went wrong'], 400);
             }
-            return back()->with('error','Something went wrong. '.$th->getMessage());
+            return back()->with('error', 'Something went wrong. ' . $th->getMessage());
         }
     }
 
@@ -64,11 +62,11 @@ class LessonController extends Controller
      */
     public function show($class_id)
     {
-        $lessons = Lesson::orderBy('date','asc')->where('class_id',$class_id)->get();
-        if(request()->is('api/*')){
-            return response()->json(['lessons'=>$lessons]);
+        $lessons = Lesson::orderBy('date', 'asc')->where('class_id', $class_id)->get();
+        if (request()->is('api/*')) {
+            return response()->json(['lessons' => $lessons]);
         }
-        return view('lessons.index',compact('lessons'));
+        return view('lessons.index', compact('lessons'));
     }
 
     /**
@@ -84,45 +82,45 @@ class LessonController extends Controller
      */
     public function update($id)
     {
-        $lesson=Lesson::findOrFail($id);
-        if(request('title')!=null){
-            $lesson->title=request('title');
+        $lesson = Lesson::findOrFail($id);
+        if (request('title') != null) {
+            $lesson->title = request('title');
         }
-        if(request('description')!=null){
-            $lesson->description=request('description');
+        if (request('description') != null) {
+            $lesson->description = request('description');
         }
-        if(request('instructor')!=null){
-            $lesson->instructor=request('instructor');
+        if (request('instructor') != null) {
+            $lesson->instructor = request('instructor');
         }
-        if(request('date')!=null){
-            $lesson->date=request('date');
+        if (request('date') != null) {
+            $lesson->date = request('date');
         }
-        if(request('venue')!=null){
-            $lesson->venue=request('venue');
+        if (request('venue') != null) {
+            $lesson->venue = request('venue');
         }
-        if(request('venue_type')!=null){
-            $lesson->venue_type=request('venue_type');
+        if (request('venue_type') != null) {
+            $lesson->venue_type = request('venue_type');
         }
-        if(request('content')!=null){
-            $lesson->content=request('content');
+        if (request('content') != null) {
+            $lesson->content = request('content');
         }
-        if(request('status')!=null){
-            $lesson->status=request('status');
+        if (request('status') != null) {
+            $lesson->status = request('status');
         }
-        if(request('content_type')!=null){
-            $lesson->content_type=request('content_type');
+        if (request('content_type') != null) {
+            $lesson->content_type = request('content_type');
         }
-        if(request('comments')!=null){
-            $lesson->comments=request('comments');
+        if (request('comments') != null) {
+            $lesson->comments = request('comments');
         }
-        if(request('created_by')!=null){
-            $lesson->created_by=request('created_by');
+        if (request('created_by') != null) {
+            $lesson->created_by = request('created_by');
         }
         $lesson->update();
-        if(request()->is('api/*')){
-            return response()->json(['message'=>'Lesson updated successfully'],200);
+        if (request()->is('api/*')) {
+            return response()->json(['message' => 'Lesson updated successfully'], 200);
         }
-        return back()->with('success','Lesson updated successfully');
+        return back()->with('success', 'Lesson updated successfully');
     }
 
     /**
@@ -133,30 +131,25 @@ class LessonController extends Controller
         //
     }
 
-    public function getData($lesson_id){
+    public function getData($lesson_id)
+    {
         // lesson, attendees, resources
-        $resources = LessonResource::where('lesson_id',$lesson_id)->select('lesson_resources.title', 'lesson_resources.path')->get();
+        $resources = LessonResource::where('lesson_id', $lesson_id)->select('lesson_resources.title', 'lesson_resources.path')->get();
         $attendance = ClassMember::where('church_class_id', Lesson::findOrFail($lesson_id)->class_id)->withExists(['attendances as is_present' => function ($query) {
             $query->whereDate('created_at', Carbon::today());
-        }])->join('users', 'users.id', '=', 'class_members.user_id')->select('users.name','users.avatar','class_members.user_id','is_present')->orderBy('name', 'asc')->get();
-        if(request()->is('api/*')){
-            return response()->json(['attendance'=>$attendance]);
+        }])->join('users', 'users.id', '=', 'class_members.user_id')->select('users.name', 'users.avatar', 'class_members.user_id', 'is_present')->orderBy('name', 'asc')->get();
+        if (request()->is('api/*')) {
+            return response()->json(['attendance' => $attendance]);
         }
-        return view('lessons.show',compact('attendance'));
+        return view('lessons.show', compact('attendance'));
     }
-    public function sendEmail($content, $subject,$id)
+    public function sendEmail($content, $subject, $id)
     {
-        $members = ClassMember::where([['church_class_id',$id], ['status', 'approved']])->get();
-        foreach($members as $member){
+        $members = ClassMember::where([['church_class_id', $id], ['status', 'approved']])->get();
+        foreach ($members as $member) {
             $user = $member->user;
-            Mail::send(
-                'message',
-                ['user' => $user->name, 'content' => $content],
-                function ($message) use ($user, $subject) {
-                    $message->to($user->email, $user->name)->subject($subject);
-                }
-            );
+            Mail::to($user->email)->queue(new RegionEmail($subject, $content));
+            return response()->json(['message' => 'Emails are being processed!']);
         }
     }
-
 }
